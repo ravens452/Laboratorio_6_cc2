dea9226bb3505b932b230b94b8047f5b7b9dad74
Carlos <candrade@unsa.edu.pe>
2020-06-20 13:17:25 -0500

    Complete

---
+++ README.md
@@ -0,0 +1,9 @@
+IMPLEMENTACIÓN
+
+1. Implemente una lista genérica simplemente enlazada.
+
+2. Implemente el patrón iterador itetator para la lista enlazadadel punto 1.
+
+3. Considere la sobrecarga de cada uno de los operadores necesarios para el uso del iterador en su lista enlazada.
+
+4. Análogo implementación de la lista list de la librería STL implemente los métodos begin y end en la lista enlazada.
--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include "Node.h"
+#include "LinkedList.h"
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+int main(){
+    cout<<"hola: "<<endl;
+    LinkedList lista1;
+    int numero;
+    cout<<"Ingrese los numeros a guardar de forma ordenada (Escriba 0 para finalizar) "<<endl;cin>>numero;
+    while (numero!=0){
+        lista1.insertarOrdenado(numero);
+        cin>>numero;
+    }
+    cout<<"Mostrando la lista sin sobrecarga de operadores"<<endl;
+    lista1.print();
+
+    cout<<"Mostrando la lista con sobrecarga de operadores"<<endl;
+    citerador it;
+    for (it=lista1.head;it!=lista1.final;it.operator++()) {
+        cout<<it.operator*()<<" ";
+    }
+
+
+
+
+
+
+//------------------------------------------------------------------------------------
+//    lista1.insert(4);
+//    lista1.insert(3);
+//    lista1.insert(5);
+//    lista1.insert(6);
+//    lista1.print();
+//    lista1.remove(4);
+//    lista1.print();
+//------------------------------------------------------------------------------------------------
+
 }
+
