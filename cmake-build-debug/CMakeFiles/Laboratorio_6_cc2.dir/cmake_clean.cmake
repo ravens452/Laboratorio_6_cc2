debe3e65dd181d0f48ca4ca17df0f3b8b6cf6f9a
Carlos <candrade@unsa.edu.pe>
2020-06-19 19:17:11 -0500

    Iniciando

---
+++ cmake-build-debug/CMakeFiles/Laboratorio_6_cc2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Laboratorio_6_cc2.dir/main.cpp.o"
+  "Laboratorio_6_cc2"
+  "Laboratorio_6_cc2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Laboratorio_6_cc2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Laboratorio_6_cc2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Laboratorio_6_cc2.
+# This may be replaced when dependencies are built.
